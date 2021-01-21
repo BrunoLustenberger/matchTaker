67e67af3a803dd787eb22ac6f3920d292936919f
bumasi <brn.lstnbrgr@gmail.com>
2021-01-21 17:09:31 +0100

    solver.solve with nested functions

--- models/solver.py
+++ models/solver.py
@@ -36,17 +36,41 @@ def solve(game_state: GameState, level: int) -> (int, int, int):
                         3 : game continues -- you have a safe strategy to win
     """
     Error.check(0 <= level <= 2, "level must be an integer in 0..2")
-    # init to you won
+    # init to" you won"
     row_index = 0
     match_count = 0
     game_continues = -1
     rows = game_state.get_rows()
+
+    # sub functions
+
+    def random_move():
+        nonlocal row_index, match_count
+        non_zeros = [k for k in range(5) if rows[k] > 0]  # all indices with value > 0
+        row_index = rand.choice(non_zeros)  # choose such index
+        max_n = min(3, rows[row_index])  # max number of matches to be taken at this index
+        if len(non_zeros) == 1:  # special case: only this row has matches --> must not take all
+            max_n = min(max_n, rows[row_index] - 1)
+        match_count = rand.randint(1, max_n)  # choose number of matches at this index
+
+    def most_first():
+        nonlocal row_index, match_count
+        """ Chooses the row with the most matches and takes as many matches as possible."""
+        p = game_state.normalize()
+        rows = game_state.get_rows()
+        match_count = min(3, rows[4])  # max number of matches
+        if rows[3] == 0:  # special case: only this row has matches --> must not take all
+            match_count = min(match_count, rows[4] - 1)
+        row_index = p.inv()(4)
+
+    # main body continued
+
     if sum(rows) > 1:
         # choose an algorithm
         if level == 0:
-            row_index, match_count = random_move(rows)
+            random_move()
         elif level == 1:
-            row_index, match_count = most_first(game_state)
+            most_first()
         else:
             pass
         assert 1 <= match_count <= min(3, rows[row_index])
@@ -59,28 +83,3 @@ def solve(game_state: GameState, level: int) -> (int, int, int):
     else:
         assert sum(rows) == 1
     return row_index, match_count, game_continues
-
-
-def random_move(rows):
-    """ Sub function of solver, parameters see there
-    """
-    non_zeros = [k for k in range(5) if rows[k] > 0]  # all indices with value > 0
-    row_index = rand.choice(non_zeros)  # choose such index
-    max_n = min(3, rows[row_index])  # max number of matches to be taken at this index
-    if len(non_zeros) == 1:  # special case: only this row has matches --> must not take all
-        max_n = min(max_n, rows[row_index] - 1)
-    match_count = rand.randint(1, max_n)  # choose number of matches at this index
-    return row_index, match_count
-
-
-def most_first(game_state):
-    """ Sub function of solver, parameters see there
-        Chooses the row with the most matches and takes as many matches as possible.
-    """
-    p = game_state.normalize()
-    rows = game_state.get_rows()
-    match_count = min(3, rows[4])  # max number of matches
-    if rows[3] == 0:  # special case: only this row has matches --> must not take all
-        match_count = min(match_count, rows[4] - 1)
-    row_index = p.inv()(4)
-    return row_index, match_count
