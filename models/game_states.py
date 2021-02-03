d0687c82e7ddce3b111ed68d635873b33799aba1
bumasi <brn.lstnbrgr@gmail.com>
2021-02-03 11:23:22 +0100

    doc-strings for game_states

--- models/game_states.py
+++ models/game_states.py
@@ -1,7 +1,15 @@
 """Module defining game-state and its operations.
 
+A game-state is a sequence of rows containing matches.
+If rows are numbered from 1 .. 5, then row n must contain 0..n matches.
+Here, rows are indexed from 0 .. 4, thus row with index k contains 0 .. k+1 matches.
+The rows are represented by a list.
 
+A game-state is called "normalized", if the rows are sorted in ascending order,
+i.e. match-count in row k <= match-count in row k+1.
 
+The lexicographic order ist defined for game-states, e.g. [1,0,0,4,5] < [1,1,0,0,0].
+Therefore, a list containing game-states, can be sorted.
 """
 
 import copy
@@ -12,19 +20,25 @@ from utils import permutation
 
 class Error(Exception):
     """Class for exceptions of this module."""
+    # todo: import from a module basic_defs
 
     @classmethod
     def check(cls, condition, *args):
+        """Check condition and raise exception if it does not hold."""
         if not condition:
             raise cls(*args)
 
 
 class GameMove:
-    """ A move in the game consists in selecting a row and taking off some matches.
+    """A move in the game consists in selecting a row and taking off some matches.
     At least 1 match and at most 3 matches must be taken.
     """
 
     def __init__(self, row_index: int, match_count: int):
+        """
+        :param row_index: index of selected row.
+        :param match_count: number of matches to take off this row
+        """
         assert row_index in range(5)
         assert match_count in range(1, 3+1)
         self.row_index = row_index
@@ -37,13 +51,13 @@ class GameMove:
             return NotImplemented
 
 
-@functools.total_ordering  # generates from == and < the other comparison operators
-# lexicographic ordering is used
+@functools.total_ordering  # uses __eq__ and __lt__ to generate the comparison operators
 class GameState:
+    """Models a game-state and its operations."""
 
     def __init__(self, rows):
-        """ Creates a game state.
-            param rows: a list of integers, representing a valid game state. Otherwise Error is raised.
+        """
+        :param rows: a list of integers, representing a valid game state. Otherwise Error is raised.
         """
         # Check and convert input
         Error.check(isinstance(rows, list), "rows must be a list", "hahaha")
@@ -64,6 +78,7 @@ class GameState:
             return NotImplemented
 
     def __lt__(self, other):
+        # lexicographic ordering is used
         if isinstance(other, self.__class__):
             return self.rows < other.rows
         else:
@@ -77,42 +92,43 @@ class GameState:
         return s
 
     def get_rows(self):
-        """ Returns a copy of the internal rows list
-        """
+        """Return a copy of the internal row list."""
         return copy.deepcopy(self.rows)
 
     def get_total_count(self):
-        """ Returns total count of all matches
-        """
+        """Return total count of all matches."""
         return sum(self.rows)
 
     def normalize(self):
-        """ Sorts the internal rows list in ascending order and returns the permutation that undoes this sorting.
-        """
+        """Sort the internal row list in ascending order and return the permutation that undoes this sorting."""
         self.rows, p = permutation.Permutation.sorted(self.rows)
         return p.inv()
 
     def is_normalized(self):
-        """ Returns True iff the internal rows list ist sorted in ascending order. """
+        """Return True iff the internal row list is sorted in ascending order."""
         test_list = [self.rows[k] <= self.rows[k+1] for k in range(4)]
         return all(test_list)
 
-    def denormalize(self, p):
-        """ Resets the internal rows list to state before normalization.
-            Param p: The permutation that was returned by the call to normalize()
+    def denormalize(self, p: permutation.Permutation):
+        """Reset the internal rows list to the state before normalization.
+        :param p: The permutation that was returned by the call to normalize()
         """
         self.rows = p.apply(self.rows)
 
     def is_possible_move(self, move: GameMove) -> bool:
-        """ Returns true iff:
-        move.match_count <= number of matches at row_index
-        and move.match_count < total count of matches (handles the case, where all matches are in 1 row)
+        """Return true iff move can be applied to self.
+        :param move: the move, that should be applied to self.
+        :return: result of the test
         """
         return (move.match_count <= self.rows[move.row_index]) and (move.match_count < sum(self.rows))
+        # Note: the 2nd condition handles the case, where all matches are in 1 row.
 
-    def make_move(self, move: GameMove):  # -> GameState:
-        """ Takes count matches off the row with index k and returns the new game state.
+    def make_move(self, move: GameMove):  # -> GameState: todo: annotation
+        """Apply the move to self and return the new game-state.
         Assumption: the move is possible.
+        More precisely: take move.match_count matches off the row index move.row_index.
+        :param move: move to apply.
+        :return: resulting game-state.
         """
         assert self.is_possible_move(move)
         new_rows = self.get_rows()
@@ -120,7 +136,7 @@ class GameState:
         return GameState(new_rows)
 
     def normalized_successors(self) -> list:
-        """
+        """Return the list of all possible normalized successors.
         Assumption: self is a normalized game state.
         :return: list of all normalized game states that can be generated from self with 1 move.
         Example: [0, 0, 1, 2, 2] will return [0, 0, 0, 2, 2], [0, 0, 1, 1, 2], [0, 0, 0, 1, 2]
@@ -140,15 +156,14 @@ class GameState:
             result = result + temp
         return result
 
-    # def get_move(self, game_state: GameState) -> (int, int):
-    # todo: annotations
+    # def get_move(self, game_state: GameState) -> GameMove: todo: annotations
     def get_move(self, game_state) -> GameMove:
-        """ Returns a move which turns self into a game state, whose normalization is equal to game_state.
+        """Return a move which turns self into an intermediate game state, whose normalization is equal to game_state.
         Example: get_move(12345,12235) == (3,2) because 12345 --> 12325 --> 12235
         Assumption: (1) self and game_state are normalized
                     (2) game_state is a successor of self
-        :param game_state: the game_state to generate with the move and following normalization
-        :return: the move
+        :param game_state: the game_state to generate with the move and following normalization.
+        :return: the move.
         """
         assert self.is_normalized()
         assert game_state.is_normalized()
