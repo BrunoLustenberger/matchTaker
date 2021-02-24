b72b9c78ce107deaab9968e958d58ae3d4f55093
bumasi <brn.lstnbrgr@gmail.com>
2021-02-24 20:54:44 +0100

    refactored

--- static/app.js
+++ static/app.js
@@ -13,7 +13,7 @@ const gameBegin = 0, userSelecting = 1, compiSelecting = 2, gameGoing = 3/*, gam
 let gameState;
 
 // rules
-let firstMoveByUser = false;
+let firstMoveByUser = true;
 
 function resetRows() {
   rows = [1,2,3,4,5];
@@ -21,10 +21,13 @@ function resetRows() {
 }
 
 function nMatches() {
-  return rows.reduce((total,num) => total + num,0);
+  return rows.reduce((total,num) => total + num, 0);
 }
 
-// for simulation
+/**
+ * Takes off some matches to simulate the move by the computer.
+ * @returns {number} - index of row, from which matches were taken
+ */
 function takeOffMatches() {
   console.assert(nMatches() > 0);
   for (let i = 4; i >= 0; i--) {
@@ -36,7 +39,10 @@ function takeOffMatches() {
   }
 }
 
-function setGameState(newState) {
+/**
+ * Perform all state-transition actions that do not depend on the previous gameState.
+ */
+function enterGameState(newState) {
   switch (newState) {
     case gameBegin:
       resetRows();
@@ -47,12 +53,19 @@ function setGameState(newState) {
       nMatchesTaken = 0;
       selectedRowIndex = -1; //undef
       rows_previous = Array.from(rows);
+      ui_message.innerText = 'click on a row and take off up to 3 matches';
       break;
     case userSelecting:
+      if (nMatchesTaken < 3) {
+        ui_message.innerText = 'Take off another match or click OK or Cancel';
+      } else {
+        ui_message.innerText = 'You took off 3 matches, click OK or Cancel';
+      }
       break;
     case compiSelecting:
       //simulate response
       console.log('fire response begin')
+      ui_message.innerText = 'Wait for the compi to take off matches';
       setTimeout(() => document.dispatchEvent(simulatedResponseEvent), 2000);
       console.log('fire response end');
       break;
@@ -60,6 +73,7 @@ function setGameState(newState) {
       console.assert(false);
   }
   gameState = newState;
+  setButtons();
 }
 
 // UI
@@ -78,6 +92,11 @@ for (let i=0; i<5; i++) {
 }
 
 // helper functions
+
+/**
+ * Shows a row.
+ * @param {Number} rowIndex - index of row.
+ */
 function showRow(rowIndex) {
   let n = rows[rowIndex];
   let s = '';
@@ -90,12 +109,18 @@ function showRow(rowIndex) {
   ui_rows[rowIndex].firstElementChild.innerHTML = s;
 }
 
+/**
+ * Shows all rows.
+ */
 function showRows() {
   for (let i = 0; i < 5; i++) {
     showRow(i);
   }
 }
 
+/**
+ * Sets the buttons depending on gameState
+ */
 function setButtons() {
   switch (gameState) {
     case gameBegin:
@@ -125,7 +150,6 @@ function setButtons() {
 }
 
 // load all event listeners
-loadEventListeners();
 
 const simulatedResponseEvent = new Event('simulatedResponse');
 
@@ -138,100 +162,119 @@ function loadEventListeners() {
   ui_Quit.addEventListener('click', quit);
 }
 
-// event listeners
+loadEventListeners();
+
+/**
+ * event listeners
+ * @param e {Event} - the event
+ */
+
+/**
+ * Handle page-load event.
+ */
 function init(e) {
   console.log(e)
-  setGameState(gameBegin);
-  setButtons();
+  enterGameState(gameBegin);
 }
 
+/**
+ * Handle the event when user clicks on a row.
+ */
 function matches(e) {
   const t = e.target;
   console.log(`${t} clicked`);
-  //console.log(t.nodeName);
+  // the event only has an effect for the following 2 states
   if (gameState === userSelecting || gameState === gameGoing) {
+    // additinally: the user must click on or near a symbol representing a match.
     if (t.nodeName === 'SPAN') {
       const p = t.parentElement;
-      let i = p.id[3];
+      let i = p.id[3]; // the char at this index gives the row-index
       //console.log(i)
       i = Number(i);
       //console.log(ui_rows[i]);
-      if (gameState === gameGoing) {
+      if (gameState === gameGoing) { // first click of this move
         selectedRowIndex = i;
       }
-      if (i === selectedRowIndex) {
-        if (nMatchesTaken < 3) {
+      if (i === selectedRowIndex) { // the same row as before was clicked, or first click
+        if (nMatchesTaken < 3) { // max not yet done
           rows[i] -= 1;
           nMatchesTaken += 1;
           showRow(i);
         } else {
-          console.log('already 3 matches taken');
+          alert('You cannot take off more than 3 matches. Click OK or Cancel!');
         }
-      } else {
-        console.log('clicked other row')
+      } else { // another row than the one before was clicked
+        alert('To take off matches from another row, click Cancel first!');
       }
-      // new state, buttons
-      setGameState(userSelecting);
-      setButtons();
+      // remain in state userSelecting or enter it
+      enterGameState(userSelecting);
+    } else {
+      console.log("clicked outside of matches");
     }
   } else {
-    console.log("row click has no effect")
+    console.log("row click has no effect");
   }
 }
 
+/**
+ * Handle the response of the computer, currently simulated.
+ */
 function response(e) {
   console.log('response begin');
   if (nMatches() > 1) {
     let i = takeOffMatches();
     showRow(i);
-    setGameState(gameGoing);
-    setButtons();
+    enterGameState(gameGoing);
   } else {
     let s = (nMatches() === 1) ? "You won!" : "You lost!";
     alert(s);
-    setGameState(gameBegin);
-    setButtons();
+    enterGameState(gameBegin);
   }
   console.log('response end');
 }
 
+/**
+ * Handle click on OK resp. Start.
+ */
 function ok(e) {
   console.log("ok clicked");
   if (gameState === gameBegin) {
     if (firstMoveByUser) {
-      setGameState(gameGoing);
+      enterGameState(gameGoing);
     } else {
-      setGameState(compiSelecting);
+      enterGameState(compiSelecting);
     }
-    setButtons();
   } else if (gameState === userSelecting) {
-      setGameState(compiSelecting);
-      setButtons();
+      enterGameState(compiSelecting);
   } else {
     console.log("ok has no effect")
   }
 }
 
+/**
+ * Handle click on Cancel.
+ */
 function cancel(e) {
   console.log("cancel clicked")
   if (gameState === userSelecting) {
     // reset selected row
     rows[selectedRowIndex] = rows_previous[selectedRowIndex];
     showRow(selectedRowIndex);
-    // new state, buttons
-    setGameState(gameGoing);
-    setButtons();
+    // new state
+    enterGameState(gameGoing);
   } else {
     console.log("cancel has no effect")
   }
 }
 
+/**
+ * Handle Click on Quit.
+ */
 function quit(e) {
   console.log("quit clicked")
   if (gameState === userSelecting || gameState === gameGoing) {
     if (confirm("Quitting means you lost the game!")) {
-      setGameState(gameBegin);
-      setButtons();
+      enterGameState(gameBegin);
     }
   } else {
     console.log("quit has no effect")
